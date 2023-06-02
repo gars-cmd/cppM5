e6259951c6f7ed82af076bf7498a4a4c004f8edb
gars-cmd <avidouille@gmail.com>
2023-06-02 19:22:33 +0300

    need to fix one test

--- sources/MagicalContainer.cpp
+++ sources/MagicalContainer.cpp
@@ -1,158 +1,279 @@
 #include "MagicalContainer.hpp"
 #include <algorithm>
-#include <complex>
+#include <cstddef>
+#include <iostream>
+#include <iterator>
+#include <stdexcept>
 #include <vector>
     
 
 ariel::MagicalContainer::MagicalContainer() noexcept {}
 
-void ariel::MagicalContainer::addElement(int new_val){
-    container.push_back(new_val);
+ariel::MagicalContainer::~MagicalContainer(){
+    for (int* pointer : this->prime_container) {
+        delete pointer;
+    }
+}
+    void ariel::MagicalContainer::sortedAdd(bool isPrime, int element){
+    if (isPrime) {
+        int* temp = new int(element);
+        this->prime_container.insert(std::upper_bound(
+            this->prime_container.begin(),
+            this->prime_container.end(),
+            temp,
+            compare), new int(element));
+        delete temp;
+    }
+    this->container.insert(std::upper_bound(
+        this->container.begin(),
+        this->container.end(),
+        element), element);
 }
 
-void ariel::MagicalContainer::removeElement(int element){
-    auto iterator = std::find(container.begin(), container.end(), element);
+bool ariel::MagicalContainer::isAlreadyIn(int element){
+    return std::find(this->container.begin(), this->container.end(), element) != this->container.end();
+}
+
+bool ariel::MagicalContainer::isPrime(int number) const{
+    // Corner cases
+    if (number <= 1){ return false;}
+    if (number <= 3){return true;}
+    if (number % 2 == 0 || number % 3 == 0){ return false;}
+    for (int index = 5; index * index <= number; index = index + 6){
+        if (number % index == 0 || number % (index + 2) == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+void ariel::MagicalContainer::addElement(int new_val){
+   if (!this->isAlreadyIn(new_val)) {
+        this->sortedAdd(this->isPrime(new_val), new_val);
+   }else {
+       std::cout << "the element is already in the container" << std::endl; 
+   }
+}
+
+
+void ariel::MagicalContainer::removeElementHelper(bool isPrime, int element) {
+    if (isPrime) {
+        for (auto it = this->prime_container.begin(); it != this->prime_container.end(); ++it) {
+            if (**it == element) {
+                delete *it;
+                this->prime_container.erase(it);
+                break;
+            }
+        }
+    }
+    auto iterator = std::find(this->container.begin(), container.end(), element);
     if (iterator != container.end()) {
         container.erase(iterator);
+    }else {
+        throw std::runtime_error("the element doesn't exist");
     }
 }
 
-int ariel::MagicalContainer::size() const{
+void ariel::MagicalContainer::removeElement(int element){
+    this->removeElementHelper(this->isPrime(element), element);
+}
+
+size_t ariel::MagicalContainer::size() const{
         return container.size();
 }
 
 ariel::MagicalContainer::AscendingIterator::AscendingIterator(const MagicalContainer& container)
-    : container(&container), index(0){}
+    : magic_container(&container), index(0){}
 
 ariel::MagicalContainer::AscendingIterator::AscendingIterator(const AscendingIterator& other)
-    :container(other.container), index(other.index){}
+    :magic_container(other.magic_container), index(other.index){}
 
 ariel::MagicalContainer::AscendingIterator::~AscendingIterator(){
 }
 
 ariel::MagicalContainer::AscendingIterator& ariel::MagicalContainer::AscendingIterator::operator=(const AscendingIterator& other){
+    if (this->magic_container->container != other.magic_container->container) {
+        throw std::runtime_error("both of the container are not the same");
+    }
+    this->magic_container = other.magic_container;
+    this->index = other.index;
     return *this;
 }
 
 bool ariel::MagicalContainer::AscendingIterator::operator==(const AscendingIterator& other) const{
-    return false;
+    return (this->magic_container == other.magic_container) && (this->index == other.index) ;
 }
 
 bool ariel::MagicalContainer::AscendingIterator::operator!=(const AscendingIterator& other) const{
-    return false;
+    return !(*this == other );
 }
 
+
+
 bool ariel::MagicalContainer::AscendingIterator::operator>(const AscendingIterator& other) const{
-    return false;
+    this->throwOnDiffContainer(other);
+    return this->index > other.index;
 }
 
 bool ariel::MagicalContainer::AscendingIterator::operator<(const AscendingIterator& other) const{
-    return false;
+    this->throwOnDiffContainer(other);
+    return this->index < other.index;
 }
 
 ariel::MagicalContainer::AscendingIterator ariel::MagicalContainer::AscendingIterator::begin() const{
-    return AscendingIterator(*container);
+    return AscendingIterator(*magic_container);
 }
 
 ariel::MagicalContainer::AscendingIterator ariel::MagicalContainer::AscendingIterator::end() const{
-    return AscendingIterator(*container, container->size());
+    return AscendingIterator(*magic_container, magic_container->size());
 }
 
 int ariel::MagicalContainer::AscendingIterator::operator*() const{
-    return 0;
+    return this->magic_container->container[this->index] ;
+}
+
+void ariel::MagicalContainer::AscendingIterator::throwOnOverIncrementation() const{
+    if (this->index >= this->magic_container->size() ) {
+        throw std::runtime_error("we already reach the end of the SideCrossIterator");
+    }
 }
 
 ariel::MagicalContainer::AscendingIterator& ariel::MagicalContainer::AscendingIterator::operator++(){
+    this->throwOnOverIncrementation();
+    ++this->index;
     return *this;
 }
 
 ariel::MagicalContainer::SideCrossIterator::SideCrossIterator(const MagicalContainer& container)
-    :container(container), index(0), fromStart(true){}
+    :magic_container(&container), index(0), offset(0){}
 
 ariel::MagicalContainer::SideCrossIterator::SideCrossIterator(const SideCrossIterator& other)
-    : container(other.container), index(other.index), fromStart(other.fromStart){}
+    : magic_container(other.magic_container), index(other.index), offset(other.offset){}
 
 ariel::MagicalContainer::SideCrossIterator::~SideCrossIterator(){}
 
 ariel::MagicalContainer::SideCrossIterator& ariel::MagicalContainer::SideCrossIterator::operator=(const SideCrossIterator& other){
+    if (this->magic_container->container != other.magic_container->container) {
+        throw std::runtime_error("both of the container are not the same");
+    }
+    this->magic_container = other.magic_container;
+    this->index = other.index;
+    this->offset = other.offset;
     return *this;
 }
 
 bool ariel::MagicalContainer::SideCrossIterator::operator==(const SideCrossIterator& other) const{
-    return false;
+    return (this->magic_container == other.magic_container) && (this->index == other.index) ;
 }
 
 bool ariel::MagicalContainer::SideCrossIterator::operator!=(const SideCrossIterator& other) const{
-    return false;
+    return !(*this == other);
 }
 
 bool ariel::MagicalContainer::SideCrossIterator::operator>(const SideCrossIterator& other) const{
-    return false;
+    this->throwOnDiffContainer(other);
+    return this->index > other.index;
 }
 
 bool ariel::MagicalContainer::SideCrossIterator::operator<(const SideCrossIterator& other) const{
-    return false;
+    this->throwOnDiffContainer(other);
+    return this->index < other.index;
 }
 
 ariel::MagicalContainer::SideCrossIterator ariel::MagicalContainer::SideCrossIterator::begin() const{
-    return SideCrossIterator(container);
+    return SideCrossIterator(*magic_container);
 }
 
 ariel::MagicalContainer::SideCrossIterator ariel::MagicalContainer::SideCrossIterator::end() const{
-    return SideCrossIterator(container, container.size(), false);
+    return SideCrossIterator(*magic_container, magic_container->size(), false);
 }
 
-int ariel::MagicalContainer::SideCrossIterator::operator*() const{
-    return 0;
+int ariel::MagicalContainer::SideCrossIterator::operator*() {
+    if (this->index % 2 == 0) {
+        return this->magic_container->container[this->index - this->offset];
+    }else {
+        this->offset++;
+        return this->magic_container->container[this->magic_container->size() - this->offset];
+    }
 }
+
+void ariel::MagicalContainer::SideCrossIterator::throwOnOverIncrementation() const{
+    if (this->index >= this->magic_container->size() ) {
+        throw std::runtime_error("we already reach the end of the SideCrossIterator");
+    }
+}
+
 
 ariel::MagicalContainer::SideCrossIterator& ariel::MagicalContainer::SideCrossIterator::operator++(){
+    this->throwOnOverIncrementation();
+    this->index++;
     return *this;
 }
 
 ariel::MagicalContainer::PrimeIterator::PrimeIterator(const MagicalContainer& container)
-    :container(container), index(0){}
+    :magic_container(&container), index(0){}
 
 ariel::MagicalContainer::PrimeIterator::PrimeIterator(const PrimeIterator& other)
-    : container(other.container), index(other.index){}
+    : magic_container(other.magic_container), index(other.index){}
 
 ariel::MagicalContainer::PrimeIterator::~PrimeIterator(){}
 
 ariel::MagicalContainer::PrimeIterator& ariel::MagicalContainer::PrimeIterator::operator=(const PrimeIterator& other){
+    if (this->magic_container->container != other.magic_container->container) {
+        throw std::runtime_error("both of the container are not the same");
+    }
+    this->magic_container = other.magic_container;
+    this->index = other.index;
     return *this;
 }
 
 bool ariel::MagicalContainer::PrimeIterator::operator==(const PrimeIterator& other) const{
-    return false;
+    return (this->magic_container == other.magic_container) && (this->index == other.index) ;
 }
 
 bool ariel::MagicalContainer::PrimeIterator::operator!=(const PrimeIterator& other) const{
-    return false;
+    return !(*this == other);
 }
 
 bool ariel::MagicalContainer::PrimeIterator::operator>(const PrimeIterator& other) const{
-    return false;
+    this->throwOnDiffContainer(other);
+    return this->index > other.index;
 }
 
 bool ariel::MagicalContainer::PrimeIterator::operator<(const PrimeIterator& other) const{
-    return false;
+    this->throwOnDiffContainer(other);
+    return this->index < other.index;
 }
 
 ariel::MagicalContainer::PrimeIterator ariel::MagicalContainer::PrimeIterator::begin() const{
-    return PrimeIterator(container);
+    return PrimeIterator(*magic_container);
 }
 
 ariel::MagicalContainer::PrimeIterator ariel::MagicalContainer::PrimeIterator::end() const{
-    return PrimeIterator(container, container.size());
+    return PrimeIterator(*magic_container, magic_container->prime_container.size());
 }
 
 int ariel::MagicalContainer::PrimeIterator::operator*() const{
-    return 0;
+    return *(this->magic_container->prime_container[this->index]) ;
+}
+
+
+void ariel::MagicalContainer::PrimeIterator::throwOnOverIncrementation() const{
+    if (this->index >= this->magic_container->prime_container.size() ) {
+        throw std::runtime_error("we already reach the end of the SideCrossIterator");
+    }
 }
 
 ariel::MagicalContainer::PrimeIterator& ariel::MagicalContainer::PrimeIterator::operator++(){
+    if (this->index >= this->magic_container->prime_container.size()) {
+        throw std::runtime_error("the index is beyond the size of the prime container");
+    }    
+    ++this->index;
     return *this;
 }
 
 //Ressources : 
 //https://www.youtube.com/watch?v=F9eDv-YIOQ0&list=PLlrATfBNZ98dudnM48yfGUldqGD0S4FFb&index=94
+//https://stackoverflow.com/questions/15843525/how-do-you-insert-the-value-in-a-sorted-vector
+//https://stackoverflow.com/questions/869593/compare-function-for-upper-bound-lower-bound
--- sources/MagicalContainer.hpp
+++ sources/MagicalContainer.hpp
@@ -1,8 +1,8 @@
 #ifndef MAGICALCONTAINER
 #define MAGICALCONTAINER
 
+#include <cstddef>
 #include <stdexcept>
-#include <string_view>
 #include <vector>
 
 
@@ -13,34 +13,46 @@ class MagicalContainer {
 private:
     std::vector<int> container;
     bool isPrime(int n) const;
-    
+    std::vector<int*> prime_container;
+    void sortedAdd(bool isPrime, int element);
+    bool isAlreadyIn(int element);
+
 public:
+    static bool compare(const int* var_a,const int* var_b){
+        return *var_a < *var_b;
+    }
+    static  bool compareEqual(const int* p, int element)  {
+        return *p == element;
+    }
+    ~MagicalContainer();
     MagicalContainer () noexcept;
     void addElement(int new_val);
-    void removeElement(int element);
-    int size() const;
+    void removeElementHelper(bool isPrime, int element);
+    void removeElement( int element);
+    size_t size() const;
     class AscendingIterator;
     class SideCrossIterator;
     class PrimeIterator;
 
-template <typename T, typename U>
-friend bool operator<(const T& left_It, const U& right_It){
-    throw std::logic_error("cannot compare two different types");
-}
-template <typename T, typename U>
-friend bool operator>(const T& left_It, const U& right_It){
-    throw std::logic_error("cannot compare two different types");
-}
+    template <typename T, typename U>
+    friend bool operator<(const T& left_It, const U& right_It){
+        throw std::logic_error("cannot compare two different types");
+    }
+    template <typename T, typename U>
+    friend bool operator>(const T& left_It, const U& right_It){
+        throw std::logic_error("cannot compare two different types");
+    }
 
 };
 
 
 class MagicalContainer::AscendingIterator {
 private:
-    const MagicalContainer* container;
-    int index;
-    AscendingIterator(const MagicalContainer& container, int index)
-        :container(&container), index(index){}
+    const MagicalContainer* magic_container;
+    size_t index;
+    AscendingIterator(const MagicalContainer& container, size_t index)
+    :magic_container(&container), index(index){}
+    void throwOnOverIncrementation() const;
 
 public:
     AscendingIterator(const MagicalContainer& container);
@@ -55,17 +67,22 @@ public:
     AscendingIterator end() const;
     int operator*() const;
     AscendingIterator& operator++();
-
+    void throwOnDiffContainer(const AscendingIterator& other) const{
+        if (this->magic_container != other.magic_container) {
+            throw std::invalid_argument("both of the container are not the same");
+        }
+    }
 };
 
 
 class MagicalContainer::SideCrossIterator {
 private:
-    const MagicalContainer& container;
-    int index;
-    bool fromStart;
-    SideCrossIterator(const MagicalContainer& container, int index, bool fromStart)
-        :container(container), index(index), fromStart(fromStart){}
+    const MagicalContainer* magic_container;
+    size_t index;
+    size_t offset;
+    SideCrossIterator(const MagicalContainer& container, size_t index, size_t offset)
+    :magic_container(&container), index(index), offset(offset){}
+    void throwOnOverIncrementation() const;
 
 public:
     SideCrossIterator(const MagicalContainer& container);
@@ -78,17 +95,23 @@ public:
     bool operator<(const SideCrossIterator& other) const;
     SideCrossIterator begin() const;
     SideCrossIterator end() const;
-    int operator*() const;
+    int operator*() ;
     SideCrossIterator& operator++();
+    void throwOnDiffContainer(const SideCrossIterator& other)const{
+        if (this->magic_container != other.magic_container) {
+            throw std::invalid_argument("both of the container are not the same");
+        }
+    }
 };
 
 
 class MagicalContainer::PrimeIterator {
 private:
-    const MagicalContainer& container;
-    int index;
-    PrimeIterator(const MagicalContainer& container, int index)
-        :container(container), index(index){}
+    const MagicalContainer* magic_container;
+    size_t index;
+    PrimeIterator(const MagicalContainer& container, size_t index)
+    :magic_container(&container), index(index){}
+    void throwOnOverIncrementation() const;
 
 public:
     PrimeIterator(const MagicalContainer& container);
@@ -103,11 +126,12 @@ public:
     PrimeIterator end() const;
     int operator*() const;
     PrimeIterator& operator++();
+    void throwOnDiffContainer(const PrimeIterator& other) const{
+        if (this->magic_container != other.magic_container) {
+            throw std::invalid_argument("both of the container are not the same");
+        }
+    }
 };
-
-
 }
-
-
 
 #endif // MAGICALCONTAINER
